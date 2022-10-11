2117fe74d76fa3fb9bda659bec25d71bd5ace3bf
hibik17 <hbtatsu@gmail.com>
2022-10-11 18:10:51 +0900

    create project & setup tailwindcss

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full h-[70px] bg-sky-300">this place is for header</div>
+</template>
---
+++ src/views/Index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import component
+import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <div class="w-full min-h-screen bg-slate-100">
+    <Header />
+  </div>
+</template>
