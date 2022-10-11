f4eaf336bd584caface440bf899f42fdbbef4b0b
hibik17 <hbtatsu@gmail.com>
2022-10-12 00:58:01 +0900

    add form component

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 // import component
 import Header from "../components/Header.vue";
+import Form from "../components/Form.vue";
 </script>
 
 <template>
   <div class="w-full min-h-screen bg-slate-100">
     <Header />
+    <div class="grid grid-cols-3">
+      <div class="col-span-1"><Form /></div>
+      <div class="col-span-2"></div>
+    </div>
   </div>
 </template>
