9cdcd90759d456ae4db25342c0e5a6bbbf01d14c
hibik17 <hbtatsu@gmail.com>
2022-10-11 18:38:13 +0900

    header copleate

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-[70px] bg-sky-300">this place is for header</div>
+  <div class="bg-sky-300 w-full h-[70px]">
+    <div class="mx-[70px] py-[10px]">
+      <ul class="flex justify-start text-white">
+        <li class="mr-6 hover:text-black">home</li>
+        <li class="mr-6">users</li>
+        <li class="mr-6">docs</li>
+      </ul>
+    </div>
+  </div>
 </template>
