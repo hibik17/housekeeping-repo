a9aff94a5906799d27107550e4448f60d9ceb59b
hibik17 <hbtatsu@gmail.com>
2022-10-12 18:15:36 +0900

    chage hover nav color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div class="bg-sky-300 w-full h-[70px]">
-    <div class="mx-[70px] py-[10px]">
-      <ul class="flex justify-start text-white">
-        <li class="mr-6 hover:text-black">home</li>
-        <li class="mr-6">users</li>
-        <li class="mr-6">docs</li>
+    <div class="mx-[70px] py-[20px]">
+      <ul class="flex justify-end text-white">
+        <li class="mr-6 hover:text-blue-500">home</li>
+        <li class="mr-6 hover:text-blue-500">users</li>
+        <li class="mr-6 hover:text-blue-500">docs</li>
       </ul>
     </div>
   </div>
