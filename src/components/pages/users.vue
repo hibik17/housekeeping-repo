a8114cce9c5772bcf88dee8386500ff6613c3457
hibik17 <hbtatsu@gmail.com>
2022-10-18 16:25:42 +0900

    add axios to get users

---
+++ src/components/pages/users.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCustomerStore } from "useCustomerStore";
+const { users } = useCustomerStore();
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
