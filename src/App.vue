f470cd806d0a9a8dedba3b1ca86725cf81fde0d2
hibik17 <hbtatsu@gmail.com>
2022-10-19 18:23:49 +0900

    resolve vite dependencies problems

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+
+import axios from "axios";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    console.log(res.data);
+  });
+});
 </script>
 
 <template>
